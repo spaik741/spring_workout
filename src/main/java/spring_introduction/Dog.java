c60de0adb373cd788ec9544fad8bd26f147a86c0
bryahchin.mihail <bryahchin.mihail@otr.ru>
2020-11-15 20:44:41 +0300

    destroy и init у Dog.java

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,6 +1,15 @@
 package spring_introduction;
 
 public class Dog implements Pet{
+
+
+    public void init (){
+        System.out.println("method init");
+    }
+
+    public void destroy (){
+        System.out.println("method destroy");
+    }
     @Override
     public void say(){
         System.out.println("Bow-Wow");
